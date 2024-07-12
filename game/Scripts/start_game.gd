a3af91aa927624bb8cced2071f2e30d917099208
Colton Phillips <coltonjphillips@gmail.com>
2024-07-11 17:50:27 -0700

    temp art required.

--- game/Scripts/start_game.gd
+++ game/Scripts/start_game.gd
@@ -1,5 +1,6 @@
 extends Node
 
+@onready var comic_button: TextureButton = $ComicButton
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,3 +14,7 @@ func _process(delta: float) -> void:
 
 func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/main.tscn")
+
+
+func _on_texture_button_pressed() -> void:
+	comic_button.visible = false
