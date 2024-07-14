6bbedf9a78ba90309697a6c124e33e4e280c05a2
Colton Phillips <coltonjphillips@gmail.com>
2024-07-13 23:41:17 -0700

    add start game skip button

--- game/Scripts/start_game.gd
+++ game/Scripts/start_game.gd
@@ -52,6 +52,9 @@ func _process(delta: float) -> void:
 func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/main.tscn")
 
+func _input(event: InputEvent) -> void:
+	if Input.is_action_just_released("ui_accept"):
+		_on_start_button_pressed()
 
 func _on_texture_button_pressed() -> void:
 	pass
