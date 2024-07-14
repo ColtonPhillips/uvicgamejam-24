fbdd75650e61cb2ee99c791a417eac782ad434b8
Colton Phillips <coltonjphillips@gmail.com>
2024-07-13 18:38:04 -0700

    add touch control

--- game/Scripts/wall.gd
+++ game/Scripts/wall.gd
@@ -1,6 +1,6 @@
 extends RigidBody2D
 class_name Wall
-
+# XXX: This doesn't work how I'd like it. wish i could adjust a shape in the main scene without affecting all walls
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
