2ab2b3d0a537475cd1d89597f1ff0d556c969c58
Colton Phillips <coltonjphillips@gmail.com>
2024-07-24 09:10:09 -0700

    fixed tsc scene issue made final buidl!

--- game/Scripts/bomb_blast.gd
+++ game/Scripts/bomb_blast.gd
@@ -9,6 +9,7 @@ func delete_me_from_this_conversation():
 	await get_tree().create_timer(0.25).timeout
 	queue_free()
 
+
 func _on_body_entered(body: Node2D) -> void:
 	if body is Brick:
 		body.decrease_level()
